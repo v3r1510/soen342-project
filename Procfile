85883ed49adb1c15bdc901883e867bedf1a44eeb
zekaran30 <zekaran30@gmail.com>
2025-10-09 17:31:29 -0400

    if you run them each works but honcho still doesnt work

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-frontend: npm --prefix frontend start
-backend: python -m flask --app backend.app --debug run -p 5000
+frontend: cmd /c "cd frontend && set PORT=3000 && npm start"
+backend:  cmd /c "set FLASK_RUN_RELOAD=0 && python -m flask --app backend.app --debug run -p 5000"
--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/message')
+def index():
+    return jsonify({"message": "here you go chocolates primates - Zidane Rakane Mansouri - 9 October 2025"})
+
+if __name__ == '__main__':
+        app.run(debug=True)
