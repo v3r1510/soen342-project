b2fb2ec3443c8c68c73ce9717c04677ff96f93ca
Valeria Rosca <valeriarosca03@gmail.com>
2025-10-10 11:34:55 -0400

    added html and css visual page for search

---
+++ frontend/public/search.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+//
+// function ResponsiveComponent() {
+//   // Add responsive logic here
+//   return <div>Responsive Content</div>;
+// }
+//
+// export default ResponsiveComponent;
