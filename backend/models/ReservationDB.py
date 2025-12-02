b40cf20660d2bf53d353763b2c5e1729f20984fd
Mohammed-Janoudi <mohamad.janoudi@gmail.com>
2025-12-02 17:34:01 -0500

    Fixed the reservtions problem / fixed the naming in the Database to be with an S

--- backend/models/ReservationDB.py
+++ backend/models/ReservationDB.py
@@ -8,7 +8,7 @@ class ReservationDB:
         if not isinstance(trip, Trip) or not isinstance(reservation, Reservation):
             raise TypeError("add_reservation expects a Trip and a Reservation")
 
-        query = "INSERT INTO Reservations (trip_id, client_id, route_id)VALUES (?, ?, ?)"
+        query = "INSERT INTO Reservations (trip_id, client_id, route_id) VALUES (?, ?, ?)"
         Database.execute_query(
             query,
             (
@@ -20,6 +20,6 @@ class ReservationDB:
 
     @staticmethod
     def get_reservations_for_trip(trip_id):
-        query = "SELECT reservation_id, trip_id, client_id, route_id FROM Reservation WHERE trip_id = ?"
+        query = "SELECT reservation_id, trip_id, client_id, route_id FROM Reservations WHERE trip_id = ?"
         rows = Database.execute_query(query, (trip_id,), fetch_all=True) or []
         return rows
